34745967f85eca058703693422ce806890520742
chaiwat pandey <chaiwat.pand@gmail.com>
2022-02-18 13:51:18 +0700

    updated intro-to-js

--- intro-to-js/govind.js
+++ intro-to-js/govind.js
@@ -9,3 +9,18 @@ function askMyName() {
     const h1message = document.getElementById("name-input").innerHTML
     alert(h1message + " King of all Pirates")
  }
+
+ function calAge() {
+   let age = prompt("What's your birthyear?");
+   age = 2022 - age;
+   alert(`“ยินดีด้วย นายอยู่ในประเทศนี้มา ${age} ปีแล้ว”`)
+
+}
+
+function loadProfile() {
+   const myName = "Govind"
+   const myProvince = "Nonthaburi"
+   document.getElementById("my-name").innerHTML = myName ;
+   document.getElementById("my-province").innerHTML = myProvince;
+
+}
