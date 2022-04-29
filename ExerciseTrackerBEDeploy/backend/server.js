bf275815ff9f12a2ff3b450ff0e7672b3a894852
chaiwat pandey <chaiwat.pand@gmail.com>
2022-04-29 10:48:56 +0700

    Initial commit of ExerciseTracker Backend part

---
+++ ExerciseTrackerBEDeploy/backend/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./routes/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+app.use(
+    session({
+        secret: "secretcode",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passports/passportConfig")(passport);
+
+
+app.use('/users', userRouter);
+
+const boot = async () => {
+    const URI = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.vwkco.mongodb.net/mernapp?retryWrites=true&w=majority'
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    app.listen(4000, () => {
+        console.log('Server is running');
+    });
+};
+
+boot();
