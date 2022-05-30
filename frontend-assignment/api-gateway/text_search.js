c72eb4432e1a13eed12b430f73028baa66728e48
chaiwat pandey <chaiwat.pand@gmail.com>
2022-05-30 15:07:00 +0700

    remove

--- frontend-assignment/api-gateway/text_search.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://localhost:9000/trips?q=https://www.wongnai.com/trips/travel-koh-cang')
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(error => {
-        console.log(error)
-    })
